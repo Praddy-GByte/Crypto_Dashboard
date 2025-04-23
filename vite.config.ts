2182ac345257df691afaf1eeb6a6c724d5740a86
Praddy5 <praddy5@Mac.home>
2025-04-23 10:44:26 -0700

    Fix Netlify build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Crypto_Dashboard/',
+  base: process.env.NODE_ENV === 'production' ? '/Crypto_Dashboard/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
